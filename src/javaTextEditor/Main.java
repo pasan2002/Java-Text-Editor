c13e10cacad271ed47077dedf0c397ce39bb80e0
Pasan Hewavitharana <115849043+pasan2002@users.noreply.github.com>
2024-05-24 20:17:03 +0530

    Initial commit

---
+++ src/javaTextEditor/Main.java
@@ -0,0 +1,10 @@
+package javaTextEditor;
+
+public class Main {
+
+	public static void main(String[] args) {
+		new Panel();
+
+	}
+
+}
---
+++ src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * 
+ */
+module JavaTextEditor {
+	requires java.desktop;
+}
